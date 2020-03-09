43a42aad3a660901698163a2acf5cda2c67cd381
Tayyab Ahmed <tayyab.ahmed@calabrio.com>
2020-03-08 22:33:10 -0700

    base is ready, now dive into redux

---
+++ src/components/courses/CoursesPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { render } from "enzyme";
+
+class CoursesPage extends React.Component {
+  render() {
+    return <h2>Courses Page</h2>;
+  }
+}
+
+export default CoursesPage;
