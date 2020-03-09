e8bfe7c60291498c3a78dbf870cfa52e46cac934
Tayyab Ahmed <tayyab.ahmed@calabrio.com>
2020-03-09 14:31:09 -0700

    completed courses page, implement redux redux store and thunk

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,10 +1,65 @@
 import React from "react";
-import { render } from "enzyme";
+import { connect } from "react-redux";
+import * as courseActions from "../../redux/actions/courseActions";
+import * as authorActions from "../../redux/actions/authorActions";
+import PropTypes from "prop-types";
+import { bindActionCreators } from "redux";
+import CourseList from "./CourseList";
 
 class CoursesPage extends React.Component {
+  componentDidMount() {
+    const { courses, authors, actions } = this.props;
+
+    if (courses.length === 0) {
+      actions.loadCourses().catch(error => {
+        alert("Loading courses failed" + error);
+      });
+    }
+
+    if (authors.length === 0) {
+      actions.loadAuthors().catch(error => {
+        alert("Loading authors failed" + error);
+      });
+    }
+  }
   render() {
-    return <h2>Courses Page</h2>;
+    return (
+      <>
+        <h2>Courses</h2>
+        <CourseList courses={this.props.courses} />
+      </>
+    );
   }
 }
 
-export default CoursesPage;
+CoursesPage.propTypes = {
+  authors: PropTypes.array.authors,
+  courses: PropTypes.array.isRequired,
+  actions: PropTypes.object.isRequired
+};
+
+function mapStateToProps(state) {
+  return {
+    courses:
+      state.authors.length === 0
+        ? []
+        : state.courses.map(course => {
+            return {
+              ...course,
+              authorName: state.authors.find(a => a.id === course.authorId).name
+            };
+          }),
+    authors: state.authors
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: {
+      loadCourses: bindActionCreators(courseActions.loadCourses, dispatch),
+      loadAuthors: bindActionCreators(authorActions.loadAuthors, dispatch)
+    }
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
